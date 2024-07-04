dd04a3e50e845c0e7f83f3f98e6ff405fd2fb119
Judy Tsai <judy.tsai@vpon.com>
2024-07-04 19:31:19 +0800

    Initial Commit

---
+++ JTCacheStore/JTCacheStore.h
@@ -0,0 +1,18 @@
+//
+//  JTCacheStore.h
+//  JTCacheStore
+//
+//  Created by Judy Tsai on 2024/7/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JTCacheStore.
+FOUNDATION_EXPORT double JTCacheStoreVersionNumber;
+
+//! Project version string for JTCacheStore.
+FOUNDATION_EXPORT const unsigned char JTCacheStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTCacheStore/PublicHeader.h>
+
+
